49d65bdc5770a47b896df293d2dce93b7c18e4b9
JavierEscuderoF <javier.escudero90@gmail.com>
2019-04-01 13:15:40 +0200

    Resolver #3

--- Refactorizacion/src/ejercicios/Refactorizacion.java
+++ Refactorizacion/src/ejercicios/Refactorizacion.java
@@ -1,9 +1,11 @@
 package ejercicios;
 
 public class Refactorizacion {
-	public double calcularPrecio(int unidades, double precioUnitario) {
+	private static final int IVA = 21;
+
+	public double calcularPrecioConIVA(int unidades, double precioUnitario) {
 		double precioSinIVA = unidades * precioUnitario;
-		double precioConIVA = precioSinIVA * 21 / 100;
+		double precioConIVA = precioSinIVA * IVA / 100;
 		return precioConIVA;
 	}
 }
