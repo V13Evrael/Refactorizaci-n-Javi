027642721f42024bd01a727dbd5f6677b1830623
José Manuel Platero <48911682+V13Evrael@users.noreply.github.com>
2019-04-05 10:45:00 +0200

    Comprobación del ejercicio de prueba de clonación y resubida de repositorios junto con refactorización

--- Refactorizacion/src/ejercicios/DNI.java
+++ Refactorizacion/src/ejercicios/DNI.java
@@ -1,10 +1,22 @@
 package ejercicios;
 
 public class DNI {
+	private static final String MENSAJE_FINAL = "La letra del DNI es ";
+	private static final String LISTA_LETRAS = "TRWAGMYFPDXBNJZSQVHLCKE";
+
 	public static void main(String[] args) {
-		String l = "TRWAGMYFPDXBNJZSQVHLCKE";
 		int n = 12345678;
-		char letra = l.charAt(n % 23);
-		System.out.println("La letra del DNI es " + letra);
+		
+		mostrarDNIConLetra(n);
+	}
+
+	private static void mostrarDNIConLetra(int dni) {
+		char letra = calculaLetraDNI(dni);
+		System.out.println(MENSAJE_FINAL + letra);
+	}
+
+	private static char calculaLetraDNI(int dni) {
+		char letra = LISTA_LETRAS.charAt(dni % 23);
+		return letra;
 	}
 }
